f3bc696b38e7df8bcdea7219881638cf4ed4922e
Eugene <eguryanov@codereign.net>
2023-02-10 17:50:53 +0300

    add jest tests

--- src/cycler.spec.ts
+++ src/cycler.spec.ts
@@ -1,22 +1,164 @@
 import { Cycler } from './cycler';
+import { Destructor } from './destructor.type';
+import { Subscription } from 'rxjs';
 
-describe('manageDestructor', () => {
-    it('should accept a destructor', () => {
-        const c = new Cycler();
+const NOOP: Destructor = () => {};
 
-        c.manageDestructor(() => {});
+const createSubscriptionSpy = () => {
+    return {
+        unsubscribe: jest.fn()
+    }
+};
+
+describe('cycler', () => {
+    describe('dispose', () => {
+        it('should dispose destructors', () => {
+            const c = new Cycler();
+
+            const mockCallback = jest.fn();
+
+            c.manageDestructor(mockCallback);
+            c.dispose();
+
+            expect(mockCallback.mock.calls).toHaveLength(1);
+        });
+
+        it('should clear queue', () => {
+            const c = new Cycler();
+
+            c.manageDestructor(() => {});
+            c.dispose();
+
+            expect(c.getDestructorsCount()).toBe(0);
+        });
+
+        it('should not dispose manually disposed destructors', () => {
+            const c = new Cycler();
+
+            const mockCallback = jest.fn();
+
+            const disposable = c.manageDestructor(mockCallback);
+            disposable.dispose();
+            c.dispose();
+
+            expect(mockCallback.mock.calls).toHaveLength(1);
+        });
     });
-});
 
-describe('dispose', () => {
-   it('should dispose destructors', () => {
-       const c = new Cycler();
+    describe('ngOnDestroy', () => {
+        it('should just call dispose', () => {
+            const c = new Cycler();
 
-       const mockCallback = jest.fn();
+            c.manageDestructor(NOOP);
+            c.ngOnDestroy();
 
-       c.manageDestructor(mockCallback);
-       c.dispose();
+            expect(c.getDestructorsCount()).toBe(0);
+        });
+    });
 
-       expect(mockCallback.mock.calls).toHaveLength(1);
-   });
+    describe('manageDestructor', () => {
+        it('should accept a destructor', () => {
+            const c = new Cycler();
+
+            c.manageDestructor(() => {});
+        });
+
+        it('should put destructor to queue', () => {
+            const c = new Cycler();
+
+            c.manageDestructor(NOOP);
+
+            expect(c.getDestructorsCount()).toBe(1);
+        });
+
+        it('should put multiple destructors', () => {
+            const c = new Cycler();
+
+            c.manageDestructor(NOOP);
+            c.manageDestructor(NOOP);
+            c.manageDestructor(NOOP);
+
+            expect(c.getDestructorsCount()).toBe(3);
+        });
+
+        describe('produced disposable', () => {
+            it('should trigger self destructor', () => {
+                const c = new Cycler();
+
+                const mockDestructor = jest.fn();
+                const disposable = c.manageDestructor(mockDestructor);
+
+                disposable.dispose();
+
+                expect(mockDestructor.mock.calls).toHaveLength(1);
+            });
+
+            it('should remove self from queue', () => {
+                const c = new Cycler();
+
+                c.manageDestructor(NOOP);
+                const mockDestructor = jest.fn();
+                const disposable = c.manageDestructor(mockDestructor);
+                c.manageDestructor(NOOP);
+
+                expect(c.getDestructorsCount()).toBe(3);
+
+                disposable.dispose();
+
+                expect(c.getDestructorsCount()).toBe(2);
+            });
+        })
+    });
+
+    describe('manageDisposable', () => {
+        it('should put item to disposal queue', () => {
+            const c = new Cycler();
+
+            c.manageDisposable({ dispose() {}});
+
+            expect(c.getDestructorsCount()).toBe(1);
+        });
+
+        it('should produce disposable', () => {
+            const c = new Cycler();
+
+            const managedDisposable = c.manageDisposable({
+                dispose() {
+                }
+            });
+
+            expect(managedDisposable).toHaveProperty('dispose');
+        });
+    });
+
+    describe('manageSubscription', () => {
+        it('should put item to disposal queue', () => {
+            const c = new Cycler();
+
+            c.manageSubscription(Subscription.EMPTY);
+
+            expect(c.getDestructorsCount()).toBe(1);
+        });
+
+        it('should unsubscribe on dispose', () => {
+            const c = new Cycler();
+
+            const subscriptionSpy = createSubscriptionSpy();
+            c.manageSubscription(subscriptionSpy);
+
+            c.dispose();
+            expect(subscriptionSpy.unsubscribe).toBeCalledTimes(1);
+        });
+
+        it('should unsubscribe on self dispose', () => {
+            const c = new Cycler();
+
+            const subscriptionSpy = createSubscriptionSpy();
+            const disposable = c.manageSubscription(subscriptionSpy);
+
+            disposable.dispose();
+
+            expect(subscriptionSpy.unsubscribe).toBeCalledTimes(1);
+        });
+    });
 });
--- src/cycler.ts
+++ src/cycler.ts
@@ -3,10 +3,18 @@ import { OnDestroy } from '@angular/core';
 import { Destructor } from './destructor.type';
 import { Observable, Subscription } from 'rxjs';
 import { finalize } from 'rxjs/operators';
+import { Unsubscribable } from 'rxjs';
 
 export class Cycler implements Disposable, OnDestroy {
     private _destructors: Destructor[] = [];
     private _transientDestructors: Record<string, Disposable> = {};
+
+    /**
+     * Gets an amount of destructors currently in the queue.
+     */
+    public getDestructorsCount(): number {
+        return this._destructors.length;
+    }
 
     /**
      * Disposes the Cycler causing all it's dependant managed
@@ -23,11 +31,83 @@ export class Cycler implements Disposable, OnDestroy {
         this._destructors = [];
     }
 
+    /**
+     * Angular lifecycle hook implementation. Expected to be
+     * called by Angular automatically. There are two ways to
+     * achieve that:
+     *
+     * ### Option 1: provide the Cycler object:
+     * ```ts
+     * import { Cycler } from 'ng-cycler';
+     *
+     * @Component({
+     *   // ...
+     *   providers: [Cycler] // provide component scope-related Cycler instance
+     * })
+     * export class MyComponent {
+     *     constructor(private readonly _cycler: Cycler){
+     *         // use _cycler wherever needed
+     *     }
+     * }
+     * ```
+     * ### Option 2: extend your component from Cycler:
+     *
+     * ```ts
+     * import { Cycler } from 'ng-cycler';
+     *
+     * export class MyComponent extends Cycler {
+     *     // use this. to call Cycler-related methods
+     * }
+     * ```
+     */
     public ngOnDestroy(): void {
         this.dispose();
     }
 
-    public manage(dependency: Destructor | Disposable | Subscription): Disposable {
+    /**
+     * Adds a Destructor to the disposal queue to be called once the Cycler
+     * is finalized by Angular.
+     *
+     * @param destructor to be added to the disposal queue
+     * @returns disposable that can be used to run the destructor at any
+     * time by calling `dispose` method. In case of `dispose` forced
+     * manually it will be excluded from the main disposal queue.
+     */
+    public manageDestructor(destructor: Destructor): Disposable {
+        this._destructors.push(destructor);
+
+        return {
+            dispose: () => {
+                const destructorIndex = this._destructors.indexOf(destructor);
+                if (destructorIndex >= 0) {
+                    destructor();
+                    this._destructors.splice(destructorIndex, 1);
+                }
+            }
+        }
+    }
+
+    /**
+     * Adds a Disposable object to the disposal queue. The logic is the same
+     * as for `manageDestructor` but using a Disposable instead of Destructor.
+     *
+     * See `manageDestructor` for details.
+     *
+     * @param disposable to be added to the disposal queue
+     *
+     * @returns disposable that wraps the original disposable. This new Disposable
+     * can be used to finalize the current one and remove it from the queue at the
+     * same time.
+     */
+    public manageDisposable(disposable: Disposable): Disposable {
+        return this.manageDestructor(() => disposable.dispose());
+    }
+
+    public manageSubscription(subscription: Unsubscribable): Disposable {
+        return this.manageDestructor(() => subscription.unsubscribe());
+    }
+
+    public manage(dependency: Destructor | Disposable | Unsubscribable): Disposable {
         const disposable = dependency as Disposable;
         if (disposable.dispose !== undefined) {
             return this.manageDisposable(disposable);
@@ -41,24 +121,6 @@ export class Cycler implements Disposable, OnDestroy {
         return this.manageDestructor(dependency as Destructor);
     }
 
-    public manageDestructor(destructor: Destructor): Disposable {
-        this._destructors.push(destructor);
-
-        return {
-            dispose: () => {
-                const destructorIndex = this._destructors.indexOf(destructor);
-                if (destructorIndex > 0) {
-                    destructor();
-                    this._destructors.splice(destructorIndex, 1);
-                }
-            }
-        }
-    }
-
-    public manageDisposable(disposable: Disposable): Disposable {
-        return this.manageDestructor(() => disposable.dispose());
-    }
-
     public manageTransient(code: string, destructor: Destructor): Disposable {
         if (this._transientDestructors[code]) {
             this._transientDestructors[code].dispose();
@@ -68,10 +130,6 @@ export class Cycler implements Disposable, OnDestroy {
         this._transientDestructors[code] = result;
 
         return result;
-    }
-
-    public manageSubscription(subscription: Subscription) {
-        return this.manageDestructor(() => subscription.unsubscribe());
     }
 
     public manageTransientSubscription(code: string, subscription: Subscription) {
